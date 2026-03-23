b57b73b4b6fabe1e7f34bd2a0855b2a201a59ab8
조성범 <cho990225@gmail.com>
2026-03-23 15:30:36 +0900

    260323

--- 01_cpp_basic/260323/260323_hw2.cpp
+++ 01_cpp_basic/260323/260323_hw2.cpp
@@ -1,25 +1,21 @@
 #include<iostream>
 using namespace std;
-
-int main()
+void ex1()
 {
-	//2. 구구단 2~9단까지의 출력을 2중 for문을 이용하여 만들어라
-	//
-
-	//for (int i = 2; i < 10; i++)
-	//{
-	//	cout << "<" << i << "단>" << endl;
-	//	for (int j = 1; j < 10; j++)
-	//	{
-	//		cout << i << " x " << j << " = " << i * j << endl;
-	//	}
-	//}
-	// 
-	//	- (심화)단과 곱을 입력받아 해당 단과 곱까지만 출력하기
-
+	for (int i = 2; i < 10; i++)
+	{
+		cout << "<" << i << "단>" << endl;
+		for (int j = 1; j < 10; j++)
+		{
+			cout << i << " x " << j << " = " << i * j << endl;
+		}
+	}
+}
+void ex2()
+{
 	int iDan = 2, iGob = 1;
 	cin >> iDan >> iGob;
-	for (int i = 2; i <= iDan+1; i++)
+	for (int i = 2; i <= iDan + 1; i++)
 	{
 		cout << "------------------------" << endl;
 		for (int j = 1; j <= iGob; j++)
@@ -27,4 +23,12 @@ int main()
 			cout << i << " x " << j << " = " << i * j << endl;
 		}
 	}
+}
+int main()
+{
+	//2. 구구단 2~9단까지의 출력을 2중 for문을 이용하여 만들어라 
+	// ex1();
+	//	- (심화)단과 곱을 입력받아 해당 단과 곱까지만 출력하기
+	// ex2();
+	
 }
