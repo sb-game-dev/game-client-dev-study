9198ec13a5d0e06a31ccb94ba9425416fdb056cd
조성범 <cho990225@gmail.com>
2026-03-24 18:46:54 +0900

    구구단 분리 및 자판기 예제 추가, 프로젝트 파일 수정
    구구단 예제(260324_hw.cpp)를 260324_hw1.cpp로 분리하였고, 자판기 시뮬레이션 예제(260324_hw2.cpp)를 새로 추가하였습니다. 이에 따라 프로젝트 파일(vcxproj, filters)에서 관련 소스 파일의 포함/제외 및 필터 설정을 반영하였습니다. 또한 260324.cpp의 메모리/변수 관련 주석을 표 형태로 보완하였습니다.

--- 01_cpp_basic/260324/260324_hw1.cpp
+++ 01_cpp_basic/260324/260324_hw1.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
-// 자판기 -> 함수로 사용 -> 입력함수, 선택함수, 거스름돈 반환 함수 등등... 함수 최대한 많이
+
 // 구구단 가로로 출력
 void gugudan();
 
 int main()
 {
 	gugudan();
+	return 0;
 }
 
 void gugudan()
@@ -18,7 +19,7 @@ void gugudan()
 			cout << i << " x " << j << " = " << i * j << "\t";
 			cout << i + 1 << " x " << j << " = " << (i + 1) * j << "\t";
 			if (i + 2 < 9)cout << i + 2 << " x " << j << " = " << (i + 2) * j;
-			cout << endl;
+			cout<<endl;
 		}
 		cout << endl;
 	}
---
+++ 01_cpp_basic/260324/260324_hw2.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int iMoney;
+bool bIsDoing = true;
+
+int MoneyInput();
+void PrintMoney();
+int ChoiceInput();
+void CheckPurchase(int choiceInput);
+
+int main()
+{
+	MoneyInput();
+	while (bIsDoing)
+	{
+		system("cls");
+		PrintMoney();
+		CheckPurchase(ChoiceInput());
+		system("pause");
+	}
+	return 0;
+}
+int MoneyInput()
+{
+	cout << "소지금을 입력해주세요:";
+	cin >> iMoney;
+	return iMoney;
+}
+
+void PrintMoney()
+{
+	cout << "잔액: "<<iMoney<<endl;
+}
+
+int ChoiceInput()
+{
+	int ichoiceInput;
+	cout << "1. 콜라(100원), 2. 사이다(200원), 3. 환타(300원), 4. 반환 : ";
+	cin >> ichoiceInput;
+	return ichoiceInput;
+}
+
+void CheckPurchase(int ichoiceInput)
+{
+	if (ichoiceInput > 4 || ichoiceInput < 1)
+	{
+		cout << "잘못된 입력입니다." << endl;
+		return;
+	}
+	if (ichoiceInput == 4)
+	{
+		bIsDoing = false;
+		system("cls");
+		cout << "거스름 돈은 "<< iMoney <<"원 입니다."<<endl;
+		return;
+	}
+	if (iMoney >= ichoiceInput * 100)
+	{
+		iMoney -= ichoiceInput *100;
+		switch (ichoiceInput)
+		{
+		case 1:
+			cout << "콜라 구매완료" << endl;
+			return;
+		case 2:
+			cout << "사이다 구매완료" << endl;
+			return;
+		case 3:
+			cout << "환타 구매완료" << endl;
+			return;
+		}
+	}
+	else cout << "잔액이 부족합니다." << endl;
+}
