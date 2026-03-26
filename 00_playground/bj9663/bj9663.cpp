f2fac891b7d9e10a081d0c6f8ccebd2ca55e9a0c
조성범 <cho990225@gmail.com>
2026-03-26 17:20:04 +0900

    bj9663/260326

---
+++ 00_playground/bj9663/bj9663.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+using namespace std;
+int n, cnt;
+int visit[16][16];
+
+bool check(int row, int col)
+{
+    for (int c = 0; c < col; c++)
+    {
+        if (visit[row][c])return false;
+    }
+
+    int r = row - 1;
+    int c = col - 1;
+    while (r >= 0 && c >= 0)
+    {
+        if (visit[r][c]) return false;
+        r--;
+        c--;
+    }
+    r = row + 1;
+    c = col - 1;
+    while (r < n && c >= 0)
+    {
+        if (visit[r][c]) return false;
+        r++;
+        c--;
+    }
+    return true;
+}
+
+void dfs(int col)
+{
+    if (col == n)
+    {
+        cnt++;
+        return;
+    }
+    for (int row = 0; row < n; row++)
+    {
+        if (check(row, col))
+        {
+            visit[row][col] = 1;
+            dfs(col + 1);
+            visit[row][col] = 0;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n;
+    dfs(0);
+    cout << cnt;
+}
