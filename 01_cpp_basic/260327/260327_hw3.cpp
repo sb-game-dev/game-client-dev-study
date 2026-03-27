3c4a99f1351401b610e61a75924bf0804c955ddb
조성범 <cho990225@gmail.com>
2026-03-27 16:13:22 +0900

    hw

--- 01_cpp_basic/260327/260327_hw3.cpp
+++ 01_cpp_basic/260327/260327_hw3.cpp
@@ -8,26 +8,6 @@ void InputAnswer(int pArr[],int iRound);
 bool Check(int pArr[], int pInput[]);
 int main()
 {
-	// 3. 야구 게임
-	/*
-	- 1 ~ 9까지의 숫자 중 무작위로 3개 추출하기
-	- 추출한 숫자를 정렬 하지 않은 상태로 매번 화면에 출력
-	
-	- 3개의 숫자를 입력 받고 입력 받은 숫자와 추출한 숫자의 자리와 값이 모두 일치하면 strike
-	- 자리를 다른데 값만 일치하면 ball
-	- 매 회 입력 시 N Strike, N Ball 을 출력
-	- 3 Strike일 시 게임 승리라고 출력 후 종료
-	- 9 라운드까지 진행을 했으나 3 Strike를 완성하지 못하면 패배라고 출력 후 종료
-	
-	7	1	4
-	
-	1	2	3	-> 1회 0 스트라이크 1볼
-	2	3	4	-> 2회 1 스트라이크 0볼
-	1	7	4	-> 3회 1 스트라이크 2볼
-	7	1	4	-> 4회 3 스트라이크 0볼
-		⁝
-	1	2	3	-> 9회 1 스트라이크 0볼 -> 패배 출력 후 종료
-	*/
 	srand(unsigned(time(NULL)));
 	int iArrSize = 0;
 	int iArr[3] = {};
