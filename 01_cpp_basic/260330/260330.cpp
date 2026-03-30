8cf744031cb96d45837c527026efbfd1b78cea91
조성범 <cho990225@gmail.com>
2026-03-30 14:04:33 +0900

    260330 class

---
+++ 01_cpp_basic/260330/260330.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+}
+// 시간 복잡도: 지수, 로그 확인
+// 
+// 2차원 배열 메모리 -> 연속된 메모리임
+// 
+// 2차원 배열의 이름을 저장하는 포인터로 따로 존재함.
+// int Arr[2][3] = {};
+// int(*pArr)[3] = Arr;
+//
