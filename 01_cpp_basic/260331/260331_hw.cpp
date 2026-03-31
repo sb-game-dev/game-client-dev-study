773d4b04d1a8ad4a333e878af60a7127fbcdaf41
cho99 <cho990225@gmail.com>
2026-03-31 16:54:11 +0900

    230331

---
+++ 01_cpp_basic/260331/260331_hw.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int StringLength(char string[]);
+void StringReverse(char string[]);
+int main()
+{
+	char cString[64] = "";
+	cin >> cString;
+	cout << "입력한 문자열 " << cString << "의 길이는 " << StringLength(cString) << "입니다." << endl;
+	
+	StringReverse(cString);
+}
+int StringLength(char string[])
+{
+	int iCnt = 0;
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		iCnt++;
+	}
+	return iCnt;
+	
+}
+void StringReverse(char string[])
+{
+	for (int i = StringLength(string) - 1; i >= 0; i--)
+	{
+		cout << string[i];
+	}
+	cout << endl;
+}
