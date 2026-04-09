73f218dd693180e2196e242720e04114ce629d81
조성범 <cho990225@gmail.com>
2026-04-09 19:33:30 +0900

    260407_hw1 수정

--- 01_cpp_basic/260407/260407_hw1.cpp
+++ 01_cpp_basic/260407/260407_hw1.cpp
@@ -8,15 +8,20 @@ int main()
 	errno_t errWrite = fopen_s(&pWritePicture, "./picture_copy.png", "wb");
 	if (errRead == 0)
 	{
+		fseek(pReadPicture, 0, SEEK_END);
+		int iSize = ftell(pReadPicture);
 		cout << "사진 파일 열기에 성공했습니다." << endl;
 		fseek(pReadPicture, 0, SEEK_SET);
-		
+		char* c = new char[iSize];
 		if (errWrite == 0)
 		{
-			while (feof(pReadPicture) == 0)
-			{
-				fputc(fgetc(pReadPicture), pWritePicture);
-			}
+			fread(c, sizeof(char), iSize, pReadPicture);
+			fwrite(c, sizeof(char), iSize, pWritePicture);
+			//while (feof(pReadPicture) == 0)
+			//{
+			//	fread(&c, sizeof(char), 1, pReadPicture);
+			//	fwrite(&c, sizeof(char), 1, pWritePicture);
+			//}
 			cout << "복사할 사진파일 생성완료." << endl;
 			fclose(pWritePicture);
 		}
