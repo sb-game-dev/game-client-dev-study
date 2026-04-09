6bcb367688d7b154ca0c86bf456012cb08e449bc
조성범 <cho990225@gmail.com>
2026-04-09 15:59:30 +0900

    bj2533, bj17090

---
+++ 00_playground/bj17090/bj17090.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int n, m, res;
+char arr[501][501];
+int dp[501][501];
+bool visit[501][501];
+bool dfs(int row, int col)
+{
+    if (dp[row][col]!=-1) return dp[row][col];
+    if (visit[row][col]) return dp[row][col] = 0;
+
+    dp[row][col] = 0;
+    visit[row][col] = true;
+    int nRow = row;
+    int nCol = col;
+    if (arr[row][col] == 'U') nRow--;
+    else if (arr[row][col] == 'R') nCol++;
+    else if (arr[row][col] == 'L')nCol--;
+    else if (arr[row][col] == 'D')nRow++;
+
+    if (nRow < 0 || nRow >= n || nCol < 0 || nCol >= m)
+    {
+        visit[row][col] = false;
+        return dp[row][col] = 1;
+    }
+    visit[row][col] = false;
+    return dp[row][col] = dfs(nRow, nCol);
+    
+}
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        for (int j = 0; j < m; j++)
+        {
+            arr[i][j] = s[j];
+        }
+    }
+    memset(dp, -1, sizeof(dp));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (dp[i][j] == -1)
+            {
+                dp[i][j] = dfs(i, j);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+     
+            if(dp[i][j] == 1) res += dp[i][j];
+            //cout << dp[i][j]<<" ";
+        }
+        //cout << "\n";
+    }
+    cout << res;
+}
+
---
+++ 00_playground/bj2533/bj2533.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#define MAX 1000001
+using namespace std;
+int n;
+vector<int> tree[MAX];
+int dp[MAX][2];
+void dfs(int node, int parent)
+{
+	dp[node][0] = 0;
+	dp[node][1] = 1;
+
+	for (int i = 0; i < tree[node].size(); i++)
+	{
+		int next = tree[node][i];
+		if (next == parent) continue;
+
+		dfs(next, node);
+
+		dp[node][0] += dp[next][1];
+		dp[node][1] += min(dp[next][0], dp[next][1]);
+	}
+}
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n-1; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		tree[a].push_back(b);
+		tree[b].push_back(a);
+	}
+
+	dfs(1,0);
+	cout << min(dp[1][0], dp[1][1]);
+}
