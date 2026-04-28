75807b2b17f7ea56f42a2f4caf46367886b08a59
조성범 <cho990225@gmail.com>
2026-04-28 15:25:54 +0900

    260428

---
+++ 02_cpp_advanced/260428/260428.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+class CObj
+{
+public:
+    CObj(string temp):m_iA(temp) {}
+    ~CObj() {}
+
+public:
+    void Render() { cout << m_iA << endl; }
+private:
+    const string m_iA;
+};
+
+
+int main()
+{
+    CObj Obj("hello");
+    Obj.Render();
+}
+
+
