5a9011fc16521f19d6497220cce66b375e86a4b6
sb-game-dev <cho990225@gmail.com>
2026-07-28 13:37:50 +0900

    260728

---
+++ 05_sr/8일차-높이맵, 1인칭 카메라 적용/Client/Code/CPlayer.cpp
@@ -0,0 +1,146 @@
+#include "pch.h"
+#include "CPlayer.h"
+#include "CProtoMgr.h"
+
+CPlayer::CPlayer(LPDIRECT3DDEVICE9 pGraphicDev)
+	: CGameObject(pGraphicDev)
+{
+}
+
+CPlayer::~CPlayer()
+{
+}
+
+HRESULT CPlayer::Ready_GameObject()
+{
+	if (FAILED(Add_Component()))
+		return E_FAIL;
+
+
+	return S_OK;
+}
+
+_int CPlayer::Update_GameObject(const _float& fTimeDelta)
+{
+
+	_int iExit = CGameObject::Update_GameObject(fTimeDelta);
+
+	return iExit;
+}
+
+void CPlayer::LateUpdate_GameObject(const _float& fTimeDelta)
+{
+	CGameObject::LateUpdate_GameObject(fTimeDelta);
+
+	Key_Input(fTimeDelta);
+}
+
+void CPlayer::Render_GameObject()
+{
+	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransformCom->Get_World());
+
+	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+
+	m_pTextureCom->Set_Texture(0);
+
+	m_pBufferCom->Render_Buffer();
+
+	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+}
+
+HRESULT CPlayer::Add_Component()
+{
+	Engine::CComponent* pComponent = nullptr;
+
+	// RcCol
+	pComponent = m_pBufferCom = dynamic_cast<CRcTex*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_RcTex"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+
+	m_mapComponent[ID_STATIC].insert({ L"Com_Buffer", pComponent });
+
+	/////////////////////////////////////////////////////////////////
+	// Transform
+	pComponent = m_pTransformCom = dynamic_cast<CTransform*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_Transform"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_Transform", pComponent });
+
+	// Texture
+	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_PlayerTexture"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+
+	m_mapComponent[ID_STATIC].insert({ L"Com_Texture", pComponent });
+
+	return S_OK;
+}
+
+void CPlayer::Key_Input(const _float& fTimeDelta)
+{
+	_vec3	vLook;
+	m_pTransformCom->Get_Info(INFO_UP, &vLook);
+
+
+	if (GetAsyncKeyState(VK_UP))
+	{
+		m_pTransformCom->Move_Pos(D3DXVec3Normalize(&vLook, &vLook), 10.f, fTimeDelta);
+	}
+
+	if (GetAsyncKeyState(VK_DOWN))
+	{
+		m_pTransformCom->Move_Pos(D3DXVec3Normalize(&vLook, &vLook), -10.f, fTimeDelta);
+	}
+
+	if (GetAsyncKeyState('Q'))
+	{
+		m_pTransformCom->Rotation(ROT_X, 180.f * fTimeDelta);
+	}
+
+	if (GetAsyncKeyState('A'))
+	{
+		m_pTransformCom->Rotation(ROT_X, -180.f * fTimeDelta);
+	}
+
+	if (GetAsyncKeyState('W'))
+	{
+		m_pTransformCom->Rotation(ROT_Y, 180.f * fTimeDelta);
+	}
+
+	if (GetAsyncKeyState('S'))
+	{
+		m_pTransformCom->Rotation(ROT_Y, -180.f * fTimeDelta);
+	}
+
+	if (GetAsyncKeyState('E'))
+	{
+		m_pTransformCom->Rotation(ROT_Z, 180.f * fTimeDelta);
+	}
+
+	if (GetAsyncKeyState('D'))
+	{
+		m_pTransformCom->Rotation(ROT_Z, -180.f * fTimeDelta);
+	}
+
+}
+
+CPlayer* CPlayer::Create(LPDIRECT3DDEVICE9 pGraphicDev)
+{
+	CPlayer* pPlayer = new CPlayer(pGraphicDev);
+
+	if (FAILED(pPlayer->Ready_GameObject()))
+	{
+		Safe_Release(pPlayer);
+		MSG_BOX("pPlayer Create Failed");
+		return nullptr;
+	}
+
+	return pPlayer;
+}
+
+void CPlayer::Free()
+{
+
+	CGameObject::Free();
+}
---
+++ 05_sr/8일차-높이맵, 1인칭 카메라 적용/Client/Code/CTerrain.cpp
@@ -0,0 +1,93 @@
+#include "pch.h"
+#include "CTerrain.h"
+#include "CProtoMgr.h"
+
+CTerrain::CTerrain(LPDIRECT3DDEVICE9 pGraphicDev)
+	: CGameObject(pGraphicDev)
+{
+}
+
+CTerrain::~CTerrain()
+{
+}
+
+HRESULT CTerrain::Ready_GameObject()
+{
+	if (FAILED(Add_Component()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+_int CTerrain::Update_GameObject(const _float& fTimeDelta)
+{
+
+	_int iExit = CGameObject::Update_GameObject(fTimeDelta);
+
+	return iExit;
+}
+
+void CTerrain::LateUpdate_GameObject(const _float& fTimeDelta)
+{
+	CGameObject::LateUpdate_GameObject(fTimeDelta);
+
+}
+
+void CTerrain::Render_GameObject()
+{
+	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransformCom->Get_World());
+
+	m_pTextureCom->Set_Texture(0);
+
+	m_pBufferCom->Render_Buffer();
+
+}
+
+HRESULT CTerrain::Add_Component()
+{
+	Engine::CComponent* pComponent = nullptr;
+
+	// TerrainTex
+	pComponent = m_pBufferCom = dynamic_cast<CTerrainTex*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_TerrainTex"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+
+	m_mapComponent[ID_STATIC].insert({ L"Com_Buffer", pComponent });
+
+	/////////////////////////////////////////////////////////////////
+	// Transform
+	pComponent = m_pTransformCom = dynamic_cast<CTransform*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_Transform"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_Transform", pComponent });
+
+	// Texture
+	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_TerrainTexture"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+
+	m_mapComponent[ID_STATIC].insert({ L"Com_Texture", pComponent });
+
+	return S_OK;
+}
+
+
+CTerrain* CTerrain::Create(LPDIRECT3DDEVICE9 pGraphicDev)
+{
+	CTerrain* pTerrain = new CTerrain(pGraphicDev);
+
+	if (FAILED(pTerrain->Ready_GameObject()))
+	{
+		Safe_Release(pTerrain);
+		MSG_BOX("pTerrain Create Failed");
+		return nullptr;
+	}
+
+	return pTerrain;
+}
+
+void CTerrain::Free()
+{
+	CGameObject::Free();
+}
